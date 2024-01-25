100ad6a3a1e53d7717feabd1871ed134b048be27
israel-b <briggs.israel@gmail.com>
2024-01-25 15:02:08 -0800

    Added update method for dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -33,6 +33,34 @@ function priceIsValid(req, res, next){
     next();
 }
 
+function ValidateDishId(req, res, next){
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id === dishId);
+    if (foundDish){
+        res.locals.dish = foundDish;
+        return next();
+    };
+    next({
+        status: 404,
+        message: `Dish does not exist: ${dishId}.`
+    });
+};
+
+function ValidateDishIdUpdate(req, res, next){
+    const { dishId } = req.params;
+    const { data: { id } = {}} = req.body;
+    if(req.body.data.id){
+        if(id != dishId) {
+            return next({
+                status: 404,
+                message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`
+            });
+        };
+    }
+    
+    next();
+}
+
 // List all dishes
 const list = (req, res) => {
     res.json({ data: dishes });
@@ -52,6 +80,22 @@ const create = (req, res) => {
     res.status(201).json({ data: newDish });
 };
 
+// Read single dish
+const read = (req, res) => {
+    res.json({ data: res.locals.dish });
+}
+
+const update = (req, res) => {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    const foundDish = res.locals.dish;
+    foundDish.name = name;
+    foundDish.description = description;
+    foundDish.price = price;
+    foundDish.image_url = image_url;
+    
+    res.json({ data: foundDish});
+}
+
 module.exports = {
     create: [
         bodyDataHas("name"),
@@ -61,5 +105,16 @@ module.exports = {
         priceIsValid,
         create
     ],
-    list,
+    read: [ValidateDishId, read],
+    update: [
+        ValidateDishId, 
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValid,
+        ValidateDishIdUpdate,
+        update
+    ],
+        list,
 };
