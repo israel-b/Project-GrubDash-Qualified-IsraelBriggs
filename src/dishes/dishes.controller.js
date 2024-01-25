c71f81c3fbdfd3508d0d88f8c0261c33290bdd29
israel-b <briggs.israel@gmail.com>
2024-01-25 14:15:36 -0800

    Added list and create routes for dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,59 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+// Middleware function to validate the request body
+function bodyDataHas(propertyName){
+    return function (req, res, next) {
+        const { data = {} } = req.body;
+        if (data[propertyName]) {
+            return next();
+        }
+        next({
+            status: 400,
+            message: `Dish must include a ${propertyName}`
+        });
+    };
+}
+
+function priceIsValid(req, res, next){
+    const { data: { price } = {} } = req.body;
+    if (price <= 0 || !Number.isInteger(price)){
+        return next({
+            status: 400,
+            message: `Dish must have a price that is an integer greater than 0`
+        });
+    }
+    next();
+}
+
+// List all dishes
+const list = (req, res) => {
+    res.json({ data: dishes });
+};
+
+// Create dishes
+const create = (req, res) => {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    const newDish = {
+        id: nextId(),
+        name,
+        description,
+        price,
+        image_url,
+    };
+    dishes.push(newDish);
+    res.status(201).json({ data: newDish });
+};
+
+module.exports = {
+    create: [
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValid,
+        create
+    ],
+    list,
+};
