52309df0dd4d702656530071c00791430469cd5f
israel-b <briggs.israel@gmail.com>
2024-01-25 16:36:21 -0800

    Completed update and delete orders

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -59,6 +59,48 @@ function validateOrderId(req, res, next){
     });
 }
 
+function validateOrderIdUpdate(req, res, next){
+    const { orderId } = req.params;
+    const { data: { id } = {} } = req.body;
+    if(req.body.data.id){
+        if(id != orderId) {
+            return next({
+                status: 404,
+                message: `Order id does not match route id. Order: ${id}, Route: ${orderId}.`
+            })
+        }
+    }
+    next();
+};
+
+function validateStatusProperty(req, res, next){
+    const { data: { status } = {}} = req.body;
+    if(!req.body.data.status){
+        return next({
+            status: 404,
+            message: `Order must have a status of pending, preparing, out-for-delivery, delivered.`
+        });
+    } else if( status === "delievered"){
+        return next({
+            status: 404,
+            message: `A delivered order cannot be changed`
+        });
+    };
+    next();
+}
+
+function validateDeleteOrderStatus(req, res, next) {
+    const { orderId } = req.params;
+    const foundOrder = orders.find((order) => order.id === orderId);
+    if(foundOrder.status !== "pending"){
+        return next({
+            status: 400,
+            message: `An order cannot be deleted unless it is pending. Returns a 400 status code`
+        });
+    }
+    next();
+}
+
 const list = (req, res) => {
     res.json({ data: orders });
 }
@@ -92,6 +134,13 @@ const update = (req, res) => {
     res.json({ data: foundOrder });
 };
 
+const destroy = (req, res) => {
+    const { orderId } = req.params;
+    const index = orders.findIndex((order) => order.id === orderId);
+    const deletedOrders = orders.splice(index, 1);
+    res.sendStatus(204);
+}
+
 module.exports = {
     create: [
         bodyDataHas("deliverTo"),
@@ -107,11 +156,14 @@ module.exports = {
         validateOrderId,
         bodyDataHas("deliverTo"),
         bodyDataHas("mobileNumber"),
-        bodyDataHas("status"),
+        //bodyDataHas("status"),
         bodyDataHas("dishes"),
         validateDishesProperty,
         validateDishQuantity,
+        validateOrderIdUpdate,
+        validateStatusProperty,
         update
     ],
+    delete: [validateOrderId, validateDeleteOrderStatus, destroy],
     list,
 };
